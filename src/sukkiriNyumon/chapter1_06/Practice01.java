1bba85f9af45128ec3434903756bf67a4b82c13b
Sanzaka <equation0815@icloud.com>
2021-11-08 10:53:46 +0900

    スッキリわかるjava入門 p296

---
+++ src/sukkiriNyumon/chapter1_06/Practice01.java
@@ -0,0 +1,11 @@
+package sukkiriNyumon.chapter1_06;
+import sukkiriNyumon.chapter1_06.comment.Zenhan;
+
+public class Practice01 {
+	public static void main(String[] args) throws Exception{
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		sukkiriNyumon.chapter1_06.comment.Kouhan.callDeae();
+		sukkiriNyumon.chapter1_06.comment.Kouhan.showMondokoro();
+	}
+}
---
+++ src/sukkiriNyumon/chapter1_06/comment/Kouhan.java
@@ -0,0 +1,14 @@
+package sukkiriNyumon.chapter1_06.comment;
+
+public class Kouhan {
+	public static void callDeae() {
+		System.out.println("ええいこしゃくな。曲者だであえ！");
+	}
+	
+	public static void showMondokoro() throws Exception {
+		System.out.println("飛車さん角さんもういいでしょう。");
+		System.out.println("この紋所が目に入らぬか！");
+		Thread.sleep(3000);
+		Zenhan.doTogame();
+	}
+}
